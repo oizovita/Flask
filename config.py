a39d8d3c959cea5ba2559ef6e59316f411160bcd
oizovita <oles1103@gmail.com>
2018-10-24 17:05:51 +0300

    postgres

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:lesclaypool@localhost/dolly'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    #SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:lesclaypool@localhost/dolly'
     basedir = os.path.abspath(os.path.dirname(__file__))
