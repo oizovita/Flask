eaf9a9afd885e71364364c586af6714cdfcd1b70
oizovita <oles1103@gmail.com>
2018-12-03 18:40:49 +0200

    connection with heroku db

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ class Configuration(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     # way to connect to the heroku database
-    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     # local database connection path
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:lesclaypool@localhost/dolly'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:lesclaypool@localhost/dolly'
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
     # folder for storing data
