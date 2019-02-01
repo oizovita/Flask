9584ff223b8638b288db7097b82619a78fbc8181
oizovita <oles1103@gmail.com>
2019-02-01 13:18:08 +0200

    the file name is taken in the table from the Name column

--- dolly.py
+++ dolly.py
@@ -14,10 +14,6 @@ FILE_HTML = 'html'
 FILE_DOCX = 'docx'
 TMP_HTML_TEMPLATE = 'template.html'
 FILE_PDF = 'pdf'
-ANNOT_KEY = '/Annots'
-ANNOT_FIELD_KEY = '/T'
-SUBTYPE_KEY = '/Subtype'
-WIDGET_SUBTYPE_KEY = '/Widget'
 
 
 # Checks if the folder already exists
@@ -69,8 +65,14 @@ def substitution_into_a_template(sheet, path_to_the_template, output_folder):
         exit()
     for i in range(1, len(sheet.col(0))):
         doc = DocxTemplate(path_to_the_template)
-        doc.render(parser_excel_file(sheet, i))
-        doc.save(output_folder + '/Invoice{0}.docx'.format(i))
+        exel_data = parser_excel_file(sheet, i)
+
+        doc.render(exel_data)
+
+        if exel_data.get('Name', False):
+            doc.save(output_folder + '/' + str(exel_data['Name']) + '.docx')
+        else:
+            doc.save(output_folder + '/Invoice{}.docx'.format(i))
 
 
 # Looks for the path to the template. Enters the necessary data into a template
@@ -86,24 +88,26 @@ def create_pdf_from_html_template(sheet, path_to_the_template, output_folder):
         stdout=subprocess.PIPE).communicate()[0].strip()
 
     pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
-
     template_html_file = path_to_the_template.split("/")[-1]
     path_to_the_template = path_to_the_template[:-len(template_html_file)]
     template = Environment(loader=FileSystemLoader(searchpath=path_to_the_template)).get_template(template_html_file)
 
     for i in range(1, len(sheet.col(0))):
-        outputText = template.render(parser_excel_file(sheet, i))
+        exel_data = parser_excel_file(sheet, i)
+        outputText = template.render(exel_data)
         html_file = open(TMP_HTML_TEMPLATE, 'w')
         html_file.write(outputText)
         html_file.close()
-        pdfkit.from_file('template.html', output_folder + '/Invoice{0}.pdf'.format(i), configuration=pdfkit_config)
+        if exel_data.get('Name', False):
+            pdfkit.from_file('template.html', output_folder + '/' + str(exel_data['Name']) + '.pdf',
+                             configuration=pdfkit_config)
+        else:
+            pdfkit.from_file('template.html', output_folder + '/Invoice{0}.pdf'.format(i), configuration=pdfkit_config)
 
     os.remove(TMP_HTML_TEMPLATE)
 
 
 def set_need_appearances_writer(writer):
-    # See 12.7.2 and 7.7.2 for more information:
-    # http://www.adobe.com/content/dam/acom/en/devnet/acrobat/pdfs/PDF32000_2008.pdf
     try:
         catalog = writer._root_object
         # get the AcroForm tree and add "/NeedAppearances attribute
@@ -156,12 +160,17 @@ def create_pdf_templates(sheet, json_file, template, output_folder):
     data_json = json.load(open(json_file))
     for i in range(1, len(sheet.col(0))):
         data_dict = {}
-        for data in data_json:
-            for exel_row in parser_excel_file(sheet, i):
-                if data_json[data] == exel_row:
-                    data_dict[data] = parser_excel_file(sheet, i)[exel_row]
+        data_execl = parser_excel_file(sheet, i)
 
-        record_in_pdf_template(template, output_folder + '/Invoice{0}.pdf'.format(i), data_dict)
+        for data in data_json:
+            for exel_row in data_execl:
+                if data_json[data] == exel_row:
+                    data_dict[data] = data_execl[exel_row]
+
+        if data_execl.get('Name', False):
+            record_in_pdf_template(template, output_folder + '/' + str(data_execl['Name']) + '.pdf', data_dict)
+        else:
+            record_in_pdf_template(template, output_folder + '/Invoice{0}.pdf'.format(i), data_dict)
 
 
 if __name__ == '__main__':
@@ -179,12 +188,14 @@ if __name__ == '__main__':
     sheet = book.sheet_by_name(book.sheet_names()[0])
 
     create_folder(args.output_folder)
+
     if args.type_file is None:
         print('Enter the file type')
         exit()
     elif args.type_file == 'd':
         substitution_into_a_template(sheet, args.template, args.output_folder)
     elif args.type_file == 'h':
-        create_pdf_from_html_template(args.template, args.output_folder)
+        create_pdf_from_html_template(sheet, args.template, args.output_folder)
     else:
         print('Incorrect key for document type entered')
+
